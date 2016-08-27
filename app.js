f535d82888d1160da088c9ad17758ee105e53cbd
June <543717080@qq.com>
2016-08-27 11:59:46 +0800

    misc: add nei

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
 global.__rootdir = __dirname;
-require("babel-register");
-require('babel-polyfill');
 module.exports = function (config) {
 	if( process.env.NODE_ENV === "development" ){
-		require('./src/server.js')(config);
+		// require('./src/server.js')(config);
+		require("babel-register");
+		require('babel-polyfill');
+		require('./src/init/index.js')(config);
 	} else {
 		require('./dist/server.js')(config);
 	}
--- test/foxman.config.js
+++ test/foxman.config.js
@@ -1,13 +1,14 @@
 var path = require('path');
 
 module.exports = {
-  port: '3000',
-  path: {
-    root: path.join(__dirname, 'ftl'),
-    syncData: path.join(__dirname, 'mock', 'fakeData'),
-    asyncData: path.join(__dirname, 'mock', 'json'),
-    static: [
-      path.join(__dirname, 'static')
-    ]
-  }
+ port: '3000',
+ neiPid:  11310,
+ path: {
+  root:      path.join(__dirname, 'ftl'),
+  syncData:  path.join(__dirname, 'mock', 'fakeData'),
+  asyncData: path.join(__dirname, 'mock', 'json'),
+  static: [
+   path.join(__dirname, 'static')
+  ]
+ }
 };
