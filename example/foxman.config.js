4de9cf4100cdd679ffcc54183ec9fe0972cb5206
xujunyu <hzxujunyu@corp.netease.com>
2016-11-02 20:21:21 +0800

    更改 vinyl-fs 的版本

--- example/foxman.config.js
+++ example/foxman.config.js
@@ -2,35 +2,26 @@
 const path = require('path');
 const mcss = require('foxman-mcss');
 const autoprefix = require('gulp-autoprefixer');
-const routers = [
-    {method: 'GET', url: '/index.html', sync: true, filePath: 'page/list'},
-    {method: 'GET', url: '/index2.html', sync: false, filePath: 'index'},
-    {method: 'GET', url: '/home/:id', sync: true, filePath: 'page/list'},
-    {method: 'POST', url: '/wxConfig.html', sync: true, filePath: 'page/wxConfig'}
-];
-let r = path.resolve;
-let alias = {
-    r: r(__dirname, 'src', 'main', 'webapp')
-};
+const routers = require('./route');
+
 
 module.exports = {
     /**
      * 如有需要，填写nei 的kei 然后执行 foxman -u 初始化工程目录。
      **/
-    nei: { 
-        key: '40708e13f4ef478871a0e548f910b29e'
-    },
+    // nei: { 
+    //     key: '40708e13f4ef478871a0e548f910b29e'
+    // },
     /**
      * task集合(基于gulp文件处理，故所有gulp插件都可用)
      * test -- String or Array<String> 需要进行转换的文件规则
      * handler -- Function 类型，需返回一个数组
      */
-    debugTool: false,
+
     preCompilers: [
         {
-            test: [r(alias.r, 'src/mcss/**/*.mcss')],
+            test: ['./src/mcss/**/*.mcss'],
             handler: (dest) => {
-                
                 return [
                     mcss({
                         "include": [],
@@ -41,7 +32,7 @@ module.exports = {
                         browsers: ['Android >= 2.3'],
                         cascade: false
                     }),
-                    dest(r(alias.r, 'src/css'))
+                    dest('./src/css')
                 ]
             }
         }
@@ -88,24 +79,23 @@ module.exports = {
          * 是否区分方法
          */
         divideMethod: false,
+        debugTool: false,
         /**
          * router type 为 sync 的filePath的ftl相对目录
          */
-        viewRoot: r(alias.r, 'WEB-INF/template'),
+        viewRoot: path.join(__dirname, 'template'),
         /**
          * router type 为 sync 的filePath的data相对目录
          */
-        syncData: r(__dirname, 'mock/fakeData'),
+        syncData: path.join(__dirname, 'mock' ,'sync'),
         /**
          * router type 为 async 的filepath的data相对目录
          */
-        asyncData: r(__dirname, 'mock/json'),
+        asyncData: path.join(__dirname, 'mock' ,'synasyncc'),
         /**
          * 静态资源目录
          */
         static: [
-            r(alias.r, 'src'),
-            r(alias.r, 'res')
         ]
     }
 };
