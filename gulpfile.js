6ce26c01ce3af324d52f6c91e662f92bad8e4a86
ImHype <543717080@qq.com>
2017-04-03 20:12:22 +0800

    移除 gulp-babel 和 gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 const gulp = require('gulp');
 const clean = require('gulp-clean');
-const babel = require('gulp-babel');
-const rename = require("gulp-rename");
 
 gulp.task('clean', function () {
     return gulp.src('dist').pipe(clean());
