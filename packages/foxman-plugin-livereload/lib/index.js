a1d0f0e102acf9943b1ea04158d734cecac0ba54
hzxujunyu <543717080@qq.com>
2017-04-15 13:47:41 +0800

    Tweaks

--- packages/foxman-plugin-livereload/lib/index.js
+++ packages/foxman-plugin-livereload/lib/index.js
@@ -1,27 +1,24 @@
 const Reloader = require('./reloader');
 
 class LivereloadPlugin {
-    constructor(options) {
-        this.options = options || {};
+    constructor() {
+        
     }
 
     init(watcherPlugin, serverPlugin) {
         const server = serverPlugin.server;
         const watcher = watcherPlugin.watcher;
+
         server.injectScript({
             condition: () => true,
             src: `/__FOXMAN__CLIENT__/js/reload.js`
         });
 
-        this.reloader = new Reloader(
-            Object.assign(
-                {
-                    watcher,
-                    server
-                },
-                this.options
-            )
-        );
+        this.reloader = new Reloader({
+            watcher,
+            server
+        });
     }
 }
+
 module.exports = LivereloadPlugin;
