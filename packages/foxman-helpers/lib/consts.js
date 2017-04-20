7ed63df1d0f6243cfcb0cb74aa872fbf288752b2
子龙 <fengzilong1992@gmail.com>
2017-04-20 21:52:01 +0800

    Tweaks

--- packages/foxman-helpers/lib/consts.js
+++ packages/foxman-helpers/lib/consts.js
@@ -7,22 +7,28 @@ exports.DispatherTypes = {
 };
 
 exports.ERRORTIPS = {
-    'NO_CONFIG': multili(
-        `Please add foxman.config.js in current directory 
+    NO_CONFIG: multili(
+        `
+        Please add foxman.config.js in current directory
         Also you can appoint your special name,
         use command 'foxman --config yourfoxman.config.js'.
-        See more in command 'foxman --help'`
+        See more in command 'foxman --help'
+        `
     ),
-    'REINSTALL': 'You can try \'npm install\' or check the foxman.config.js',
-    'MAYBE_FOXMAN_CONFIG': multili(
-        `Make sure you have the latest version of node.js and foxman.
+    REINSTALL: `You can try 'npm install' or check the foxman.config.js`,
+    MAYBE_FOXMAN_CONFIG: multili(
+        `
+        Make sure you have the latest version of node.js and foxman.
         If you do, this is most likely a problem with the plugins used in your foxman.config.js,
-        not with foxman itself`
+        not with foxman itself
+        `
     ),
-    'INSTALL_LATEST_FOXMAN': multili(
-        `Please install latest version:',
+    INSTALL_LATEST_FOXMAN: multili(
+        `
+        Please install latest version:',
         $ npm i -g foxman
         And re-install all dependencies in current working directory
-        For more release information, head to https://github.com/kaola-fed/foxman/releases ;-)`
+        For more release information, head to https://github.com/kaola-fed/foxman/releases ;-)
+        `
     )
 };
