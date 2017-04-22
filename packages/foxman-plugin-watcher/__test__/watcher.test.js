d22b54c0c6956262bb4934f8769f28725bb4740a
子龙 <fengzilong1992@gmail.com>
2017-04-22 18:19:02 +0800

    Fix watcher tests

--- packages/foxman-plugin-watcher/__test__/watcher.test.js
+++ packages/foxman-plugin-watcher/__test__/watcher.test.js
@@ -1,52 +1,52 @@
-const Watcher = require('../lib')
+const Watcher = require('../lib');
 const watcher = new Watcher();
 const path = require('path');
-const {fs} = require('@foxman/helpers');
+const { fs } = require('@foxman/helpers');
 const filepath = path.join(__dirname, 'fixtures', 'foo.txt');
 
-test('createWatcher', (done) => {
+test('createWatcher', done => {
     const _watcher = watcher._createWatcher({
         files: filepath
     });
 
-    return new Promise((resolve) => {
-        _watcher.on('change', (file) => {
+    return new Promise(resolve => {
+        _watcher.on('change', file => {
             _watcher.close();
             resolve();
             expect(file).toBe(filepath);
             done();
         });
 
-        fs.write(filepath, 'Foo Bar');
+        fs.write(filepath, 'Foo Bar' + new Date().getTime());
     });
 });
 
-test('createWatcher2', (done) => {
+test('createWatcher2', done => {
     const _watcher = watcher._createWatcher(filepath);
 
-    return new Promise((resolve) => {
-        _watcher.on('change', (file) => {
+    return new Promise(resolve => {
+        _watcher.on('change', file => {
             _watcher.close();
             resolve();
             expect(file).toBe(filepath);
             done();
         });
 
-        fs.write(filepath, 'Foo Bar');
+        fs.write(filepath, 'Foo Bar' + new Date().getTime());
     });
 });
 
-test('createWatcher3', (done) => {
+test('createWatcher3', done => {
     const _watcher = watcher._createWatcher([filepath]);
 
-    return new Promise((resolve) => {
-        _watcher.on('change', (file) => {
+    return new Promise(resolve => {
+        _watcher.on('change', file => {
             _watcher.close();
             resolve();
             expect(file).toBe(filepath);
             done();
         });
 
-        fs.write(filepath, 'Foo Bar');
+        fs.write(filepath, 'Foo Bar' + new Date().getTime());
     });
 });
