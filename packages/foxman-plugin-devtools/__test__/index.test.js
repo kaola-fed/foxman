cfea90d7bc76ac71b677a17fe76a86505e7b3b5b
hzfengzilong <feng_zilong@163.com>
2017-04-27 16:27:00 +0800

    test: Add more tests

--- packages/foxman-plugin-devtools/__test__/index.test.js
+++ packages/foxman-plugin-devtools/__test__/index.test.js
@@ -3,4 +3,13 @@ const DevtoolsPlugin = require( '../lib' );
 test( 'is foxman plugin', () => {
 	var plugin = new DevtoolsPlugin();
 	expect(typeof plugin.init).toBe( 'function' );
+
+	const mock = jest.fn();
+
+	plugin.init( {
+		service: mock,
+	} );
+
+	expect( mock ).toHaveBeenCalled();
+	expect( typeof plugin.name() ).toBe( 'string' );
 } );
