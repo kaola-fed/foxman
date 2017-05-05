b337375392ca296ebe1b21835c4b7e5456257956
hzxujunyu <543717080@qq.com>
2017-05-05 21:20:38 +0800

    feat: init foxman-engine-handlebars

---
+++ packages/foxman-engine-handlebars/lib/index.js
@@ -0,0 +1,13 @@
+// const handlebars = require('handlebars');
+
+// class Handlebars {
+//     constructor(root, engineConfig = {}) {
+        
+//     }
+
+//     parse(filepath, data) {
+        
+//     }
+// }
+
+// module.exports = Handlebars;
