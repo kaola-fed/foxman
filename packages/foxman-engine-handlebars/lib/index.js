0d827a7535466f08d4a1947f1d806340cf13395c
hzxujunyu <543717080@qq.com>
2017-05-09 14:43:22 +0800

    feat: init @foxman/engine-handlebars

--- packages/foxman-engine-handlebars/lib/index.js
+++ packages/foxman-engine-handlebars/lib/index.js
@@ -1,13 +1,16 @@
-// const handlebars = require('handlebars');
+const co = require('co');
+const Renderer = require('./renderer.js');
 
-// class Handlebars {
-//     constructor(root, engineConfig = {}) {
-        
-//     }
+class Handlebars {
+    constructor(root, engineConfig = {}) {
+        this.renderer = Renderer(Object.assign({
+            root
+        }, engineConfig));
+    }
 
-//     parse(filepath, data) {
-        
-//     }
-// }
+    parse(filepath, data) {
+        return co(this.renderer.render(filepath, data));
+    }
+}
 
-// module.exports = Handlebars;
+module.exports = Handlebars;
