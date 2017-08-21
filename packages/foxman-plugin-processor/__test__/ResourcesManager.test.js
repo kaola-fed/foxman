53be2d183343f38823639a6d25203acda9e68916
ImHype <543717080@qq.com>
2017-08-21 11:48:20 +0800

    chore: update test cases

--- packages/foxman-plugin-processor/__test__/ResourcesManager.test.js
+++ packages/foxman-plugin-processor/__test__/ResourcesManager.test.js
@@ -4,23 +4,23 @@ const co = require('co');
 const resourcesManager = new ResourcesManager();
 
 test('set', () => {
-    resourcesManager.set({ 
-        reqPath: '/1', 
-        processed: '1'
+    resourcesManager.set('/1', { 
+        version: '1xxd',
+        content: '1'
     });
-    expect(resourcesManager._map['/1']).toBe('1')
+    expect(resourcesManager._map['/1'].content).toBe('1')
 })
 
 test('get', () => {
-    expect(resourcesManager.get('/1')).toBe('1');
+    expect(resourcesManager.get('/1').content).toBe('1');
 })
 
 
 test('has', () => {
-    expect(resourcesManager.has('/1')).toBe(true);
+    expect(!!resourcesManager.has('/1')).toBe(true);
 })
 
 test('clear', () => {
-    resourcesManager.clear()
-    expect(resourcesManager.has('/1')).toBe(true);
+    resourcesManager.clear('/1')
+    expect(!!resourcesManager.has('/1')).toBe(false);
 })
